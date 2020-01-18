2b490fc9f9eee6f47f0042a575dedd1be28c8e51
Gabriel Soares <gssoares@arezzo.com.br>
2020-01-18 07:10:14 -0300

    Add yup to the project.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    const validate = await schema.isValid(req.body);
+
+    // if (!schema.isValid(req.body)) {
+    if (!validate) {
+      return res.status(400).json({ error: 'Required Fields' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
